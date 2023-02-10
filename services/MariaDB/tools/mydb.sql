854e2b95c82beecf803a7a817de908e0d65b27b8
bouqssim1 <hamzaboukssim6@gmail.com>
2023-02-10 07:20:12 +0100

    error: still redirecting to nginx page not WP

--- services/MariaDB/tools/mydb.sql
+++ services/MariaDB/tools/mydb.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE if not exists wordpress;
-CREATE USER 'hbouqssi'@'%';
-GRANT ALL PRIVILEGES ON my_database.* to 'hbouqssi'@'%' IDENTIFIED BY '0000';
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER 'hbouqssi'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON wordpress.* to 'hbouqssi'@'localhost' IDENTIFIED BY '0000';
 ALTER USER 'root'@'localhost' identified by '0000';
 FLUSH PRIVILEGES;
