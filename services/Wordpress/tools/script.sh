617f07f07d0b9da4ba274c841f16df0b347f0c5f
bouqssim1 <hamzaboukssim6@gmail.com>
2023-02-11 04:25:47 +0100

    still got the same problem: wp not showing up

--- services/Wordpress/tools/script.sh
+++ services/Wordpress/tools/script.sh
@@ -12,4 +12,20 @@ mv wp-config-sample.php wp-config.php
 wp config set DB_NAME "wordpress" --allow-root
 wp config set DB_USER "hbouqssi" --allow-root
 wp config set DB_PASSWORD "0000" --allow-root
-exec "$@"
+
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+
+wp theme install astra --activate --allow-root
+
+
+wp plugin install redis-cache --activate --allow-root
+
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
+
+mkdir /run/php
+
+
+/usr/sbin/php-fpm7.3 -F
