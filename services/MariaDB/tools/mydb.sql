ea0bc1314d50505b2dbefa1f72415f62973e51de
Hamza Bouqssim <hbouqssi@student.1337.ma>
2023-02-14 21:32:20 +0100

    fixed: error establishing connetion

--- services/MariaDB/tools/mydb.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER 'hbouqssi'@'wordpress.inception';
-GRANT SELECT, INSERT, UPDATE, DELETE ON wordpress.* to 'hbouqssi'@'wordpress.inception' IDENTIFIED BY '0000';
-ALTER USER 'root'@'localhost' identified by '0000';
-FLUSH PRIVILEGES;
--- services/MariaDB/tools/script.sh
+++ services/MariaDB/tools/script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
 service mysql start
-mysql < /mydb.sql
-#rm -f /mydb.sql
+
+mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+
+mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+                    GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+                    FLUSH PRIVILEGES;"
+
+kill `cat /var/run/mysqld/mysqld.pid`
+
+exec "$@"
