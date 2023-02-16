5d00de6c7af9a386586704bf3dc15fd7274fcd0d
Hamza Bouqssim <hbouqssi@student.1337.ma>
2023-02-16 21:58:46 +0100

    skipped wp installation page

--- services/Wordpress/tools/script.sh
+++ services/Wordpress/tools/script.sh
@@ -18,5 +18,7 @@ wp config set DB_NAME "wordpress" --allow-root
 wp config set DB_USER "hbouqssi" --allow-root
 wp config set DB_PASSWORD "0000" --allow-root
 wp config set DB_HOST "mariadb" --allow-root
+wp core install  -path=/var/www/html --url="localhost" --title="kradagh" --admin_user="supervisor" --admin_password=0000 --admin_email="hamzaboukssim6@gmail.com" --allow-root
+wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 mkdir /run/php
 /usr/sbin/php-fpm7.3 -F
