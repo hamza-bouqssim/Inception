6bf64c9de8d83178dd518dc8d095acaad12fefc4
Hamza Bouqssim <hbouqssi@student.1337.ma>
2023-02-19 22:58:22 +0100

    redis: done

--- services/Wordpress/tools/script.sh
+++ services/Wordpress/tools/script.sh
@@ -18,7 +18,13 @@ wp config set DB_NAME "wordpress" --allow-root
 wp config set DB_USER "hbouqssi" --allow-root
 wp config set DB_PASSWORD "0000" --allow-root
 wp config set DB_HOST "mariadb" --allow-root
+wp config set WP_REDIS_HOST "redis" --allow-root
+wp config set WP_REDIS_PORT "6379" --allow-root
+wp config set WP_CACHE "true" --allow-root
+# wp config set --allow-root WP_CACHE_KEY_SALT $DOMAIN_NAME
 wp core install  -path=/var/www/html --url="localhost" --title="kradagh" --admin_user="supervisor" --admin_password=0000 --admin_email="hamzaboukssim6@gmail.com" --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+wp wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
 mkdir /run/php
 /usr/sbin/php-fpm7.3 -F
